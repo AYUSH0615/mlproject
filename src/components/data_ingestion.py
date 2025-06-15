f87702581940db0647a6465119eebd192b2625a9
Ayush Srivastava <ayushsrivastava0615@gmail.com>
2025-06-15 08:59:43 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,6 @@
 import os 
 import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -8,6 +9,10 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationconfig
+from src.components.model_trainer import ModelTrainerconfig
+from src.components.model_trainer import ModelTrainer
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -48,5 +53,7 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion() 
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
-            
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
